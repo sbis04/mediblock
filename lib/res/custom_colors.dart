cd293f57adebc1cfea9ad3e8baadddad17bd8f39
Souvik Biswas <sbis1999@gmail.com>
2020-12-03 23:17:00 +0530

    Add color palette

---
+++ lib/res/custom_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class CustomColors {
+  static final Color black = Color(0xFF0E0F19);
+  static final Color brown = Color(0xFF433A3F);
+  static final Color yellow = Color(0xFFFFB400);
+  static final Color blue = Color(0xFF2EC4B6);
+  static final Color red = Color(0xFFFF3366);
+}
