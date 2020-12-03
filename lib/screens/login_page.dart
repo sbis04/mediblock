a3a3f425666a1ec495fed1edffe2c193b7fbb719
Souvik Biswas <sbis1999@gmail.com>
2020-12-03 23:44:45 +0530

    Add login screen

--- lib/screens/login_page.dart
+++ lib/screens/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mediblock/res/custom_colors.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -6,8 +7,120 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  static const double TITLE_TEXT_SIZE = 50;
+
+  bool _isLoggingIn = false;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: CustomColors.dark,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.only(
+            left: 16.0,
+            right: 16.0,
+            bottom: 20.0,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Row(),
+              Expanded(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    RichText(
+                      text: TextSpan(
+                        text: 'M',
+                        style: TextStyle(
+                          color: CustomColors.yellow,
+                          fontSize: TITLE_TEXT_SIZE,
+                        ),
+                        children: [
+                          TextSpan(
+                            text: 'edi',
+                            style: TextStyle(
+                              color: CustomColors.yellow.withOpacity(0.8),
+                              fontSize: TITLE_TEXT_SIZE,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'B',
+                            style: TextStyle(
+                              color: CustomColors.blue,
+                              fontSize: TITLE_TEXT_SIZE,
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'lock',
+                            style: TextStyle(
+                              color: CustomColors.blue.withOpacity(0.8),
+                              fontSize: TITLE_TEXT_SIZE,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              _isLoggingIn
+                  ? Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: CircularProgressIndicator(
+                        valueColor: new AlwaysStoppedAnimation<Color>(
+                          CustomColors.blue,
+                        ),
+                      ),
+                    )
+                  : Padding(
+                      padding: EdgeInsets.only(left: 20.0, right: 20.0),
+                      child: Container(
+                        width: double.maxFinite,
+                        child: RaisedButton(
+                          color: CustomColors.blue,
+                          onPressed: () async {
+                            setState(() {
+                              _isLoggingIn = true;
+                            });
+                            // await signInWithGoogle().then((result) {
+                            //   if (result != null) {
+                            //     Navigator.of(context).pushReplacement(
+                            //       MaterialPageRoute(
+                            //         builder: (context) => NamePage(),
+                            //       ),
+                            //     );
+                            //   }
+                            // }).catchError((e) => print('Google sign in error: $e'));
+                            // setState(() {
+                            //   _isLoggingIn = false;
+                            // });
+                          },
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+                            child: Text(
+                              'LOGIN',
+                              style: TextStyle(
+                                fontFamily: 'Raleway',
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                letterSpacing: 2,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
