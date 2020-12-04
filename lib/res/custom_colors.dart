1521d61eb184bf21eb8d8ea854263af919f5936e
Souvik Biswas <sbis1999@gmail.com>
2020-12-04 22:06:26 +0530

    Update colors

--- lib/res/custom_colors.dart
+++ lib/res/custom_colors.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class CustomColors {
-  static final Color dark = Color(0xF2F4858);
+  static final Color dark = Color(0xFF2F4858);
   static final Color brown = Color(0xFF433A3F);
+  static final Color caput = Color(0xFF5C1A1B);
   static final Color yellow = Color(0xFFFFB400);
   static final Color blue = Color(0xFF2EC4B6);
   static final Color red = Color(0xFFFF3366);
