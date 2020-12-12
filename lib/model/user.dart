1b9b343458e49f186a2b17a578fa9202d52a5e20
Souvik Biswas <sbis1999@gmail.com>
2020-12-12 21:40:39 +0530

    Add a new user property

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -3,15 +3,18 @@ import 'package:flutter/material.dart';
 class User {
   String uid;
   String name;
+  int waiting;
 
   User({
     @required this.uid,
     @required this.name,
+    @required this.waiting,
   });
 
   User.fromJson(Map<String, dynamic> json) {
     uid = json['uid'];
     name = json['name'];
+    waiting = json['waiting'];
   }
 
   Map<String, dynamic> toJson() {
@@ -19,6 +22,7 @@ class User {
 
     data['uid'] = this.uid;
     data['name'] = this.name;
+    data['waiting'] = this.waiting;
 
     return data;
   }
